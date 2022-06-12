41d11b6d6ee9b4c76167d5535472449e73e4a508
Igor Baliuk <baliuk.igor@gmail.com>
2022-06-12 20:03:07 +0300

    Save query execution time and when it was created

--- internal/queryrun/run.go
+++ internal/queryrun/run.go
@@ -1,20 +1,27 @@
 package queryrun
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Run struct {
 	ID string `dynamodbav:"Id"`
 
 	Version string `dynamodbav:"Version"`
+	Input   string `dynamodbav:"Input"`
+	Output  string `dynamodbav:"Output"`
 
-	Input  string `dynamodbav:"Input"`
-	Output string `dynamodbav:"Output"`
+	CreatedAt     time.Time     `dynamodbav:"CreatedAt"`
+	ExecutionTime time.Duration `dynamodbav:"ExecutionTime"`
 }
 
 func New(input string, version string) *Run {
 	return &Run{
-		ID:      uuid.New().String(),
-		Input:   input,
-		Version: version,
+		ID:        uuid.New().String(),
+		CreatedAt: time.Now(),
+		Input:     input,
+		Version:   version,
 	}
 }
