3afc9d32abac2b55009b9233bcb821b537db3713
Igor Baliuk <baliuk.igor@gmail.com>
2022-06-19 22:10:32 +0300

    Test concurrency limits in balancer

--- internal/qrunner/coordinator/balancer_test.go
+++ internal/qrunner/coordinator/balancer_test.go
@@ -4,13 +4,77 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"clickhouse-playground/internal/qrunner/stubrunner"
 
+	"github.com/rs/zerolog"
 	zlog "github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestBalancer_processJob_ConcurrencyLimitExhausted(t *testing.T) {
+	ctx := context.Background()
+	maxConcurrency := uint32(5)
+
+	r1 := NewRunner(stubrunner.New(ctx, "runner_1", stubrunner.StubRun), 100, &maxConcurrency)
+	r2 := NewRunner(stubrunner.New(ctx, "runner_2", stubrunner.StubRun), 300, &maxConcurrency)
+
+	b := newBalancer(zlog.Logger.Level(zerolog.ErrorLevel))
+	assert.True(t, b.add(r1))
+	assert.True(t, b.add(r2))
+
+	const iterations = 1000
+
+	for i := 0; i < iterations; i++ {
+		jobsCreated := sync.WaitGroup{}
+		jobsCompleted := sync.WaitGroup{}
+		initFinished, finishInit := context.WithCancel(context.Background())
+
+		r1Selected := new(uint32)
+		r2Selected := new(uint32)
+
+		for j := uint32(0); j < *r1.maxConcurrency+*r2.maxConcurrency; j++ {
+			jobsCreated.Add(1)
+			jobsCompleted.Add(1)
+
+			go func() {
+				defer jobsCompleted.Done()
+
+				processed := b.processJob(func(r *Runner) {
+					jobsCreated.Done()
+					<-initFinished.Done()
+
+					switch r {
+					case r1:
+						atomic.AddUint32(r1Selected, 1)
+					case r2:
+						atomic.AddUint32(r2Selected, 1)
+					default:
+						panic("unknown runner")
+					}
+				})
+
+				assert.True(t, processed)
+			}()
+		}
+
+		jobsCreated.Wait()
+
+		for j := 0; j < 10; j++ {
+			processed := b.processJob(func(r *Runner) {})
+			assert.False(t, processed)
+		}
+
+		finishInit()
+		jobsCompleted.Wait()
+
+		assert.Equal(t, *r1.maxConcurrency, *r1Selected)
+		assert.Equal(t, *r2.maxConcurrency, *r2Selected)
+	}
+}
 
 func TestBalancer_selectRunner_EqualWeights(t *testing.T) {
 	const runnerCount = 5
