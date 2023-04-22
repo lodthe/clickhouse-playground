031f925f4535f8352400ae24e34c7e4f744be4eb
Igor Baliuk <baliuk.igor@gmail.com>
2023-04-22 16:16:55 +0200

    Observe prewarmer metrics

--- internal/qrunner/dockerengine/prewarmer.go
+++ internal/qrunner/dockerengine/prewarmer.go
@@ -1,6 +1,7 @@
 package dockerengine
 
 import (
+	"clickhouse-playground/internal/metrics"
 	"context"
 	"fmt"
 	"sync"
@@ -18,6 +19,7 @@ type prewarmer struct {
 	ctx    context.Context
 	cancel context.CancelFunc
 	logger zerolog.Logger
+	metr   *metrics.PrewarmerExporter
 
 	runner containerRunner
 	engine *engineProvider
@@ -38,6 +40,7 @@ func newPrewarmer(ctx context.Context, logger zerolog.Logger, runner containerRu
 		ctx:               ctx,
 		cancel:            cancel,
 		logger:            logger,
+		metr:              metrics.NewPrewarmerExporter(),
 		runner:            runner,
 		engine:            engine,
 		containers:        make(map[string]*containerState),
@@ -96,6 +99,8 @@ func (p *prewarmer) Stop(shutdownCtx context.Context) {
 		err := p.engine.removeContainer(shutdownCtx, c.id, true)
 		if err != nil {
 			p.logger.Err(err).Str("container_id", c.id).Msg("failed to remove container")
+		} else {
+			p.metr.EjectContainer()
 		}
 	}
 
@@ -134,8 +139,6 @@ func (p *prewarmer) runContainer(request *requestState) error {
 		return fmt.Errorf("failed to create a new container: %w", err)
 	}
 
-	p.logger.Info().Str("id", state.containerID).Str("image", state.imageFQN).Msg("a new prewarmed container has been created")
-
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
@@ -155,24 +158,7 @@ func (p *prewarmer) runContainer(request *requestState) error {
 	// If the number of prewarmed containers exceeds the limit,
 	// delete the oldest.
 	if len(p.containers) > int(p.maxWarmContainers) {
-		oldestImage := container.imageFQN
-		oldestDate := container.createdAt
-		for fqn, c := range p.containers {
-			if c.createdAt.Before(oldestDate) {
-				oldestImage = fqn
-				oldestDate = c.createdAt
-			}
-		}
-
-		oldestContainer := p.containers[oldestImage]
-		delete(p.containers, oldestImage)
-
-		go func() {
-			err := p.engine.removeContainer(p.ctx, oldestContainer.id, true)
-			if err != nil {
-				p.logger.Err(err).Str("container_id", oldestContainer.id).Msg("failed to remove container")
-			}
-		}()
+		p.ejectContainer()
 	}
 
 	// Pause container after some time to allow its bootstrap.
@@ -192,10 +178,40 @@ func (p *prewarmer) runContainer(request *requestState) error {
 
 		container.setStatus(statusPaused)
 
-		p.logger.Info().Str("id", container.id).Str("image", container.imageFQN).Msg("container has been paused")
+		p.logger.Debug().Str("id", container.id).Str("image", container.imageFQN).Msg("container has been paused")
 	}()
 
+	p.metr.AddContainer()
+	p.logger.Debug().Str("id", container.id).Str("image", container.imageFQN).
+		Msg("a new container has been added to the prewarmed set")
+
 	return nil
+}
+
+// ejectContainer removes the oldest container to allow a new container to be created.
+func (p *prewarmer) ejectContainer() {
+	var oldestImage string
+	oldestDate := time.Now()
+	for fqn, c := range p.containers {
+		if c.createdAt.Before(oldestDate) {
+			oldestImage = fqn
+			oldestDate = c.createdAt
+		}
+	}
+
+	container := p.containers[oldestImage]
+	delete(p.containers, oldestImage)
+
+	p.metr.EjectContainer()
+	p.logger.Debug().Str("id", container.id).Str("image", container.imageFQN).
+		Msg("a container has been ejected from the prewarmed set")
+
+	go func() {
+		err := p.engine.removeContainer(p.ctx, container.id, true)
+		if err != nil {
+			p.logger.Err(err).Str("container_id", container.id).Msg("failed to remove container")
+		}
+	}()
 }
 
 // PushNewRequest should be called when a new request comes.
@@ -226,6 +242,9 @@ func (p *prewarmer) PushNewRequest(request requestState) {
 func (p *prewarmer) Fetch(imageFQN string) (containerID string, found bool, err error) {
 	c := p.extractContainer(imageFQN)
 	if c == nil {
+		p.metr.FetchMiss()
+		p.logger.Debug().Str("image", imageFQN).Msg("prewarmer cache miss")
+
 		return "", false, nil
 	}
 
@@ -233,6 +252,9 @@ func (p *prewarmer) Fetch(imageFQN string) (containerID string, found bool, err 
 	if err != nil {
 		return "", false, err
 	}
+
+	p.metr.FetchHit()
+	p.logger.Debug().Str("id", c.id).Str("image", c.imageFQN).Msg("prewarmer cache hit")
 
 	return c.id, true, nil
 }
@@ -247,6 +269,10 @@ func (p *prewarmer) extractContainer(imageFQN string) (container *containerState
 	}
 
 	delete(p.containers, imageFQN)
+
+	p.metr.FetchContainer()
+	p.logger.Debug().Str("id", container.id).Str("image", container.imageFQN).
+		Msg("a container has been fetched from the prewarmed set")
 
 	return container
 }
@@ -264,7 +290,7 @@ func (p *prewarmer) unpauseIfNecessary(c *containerState) error {
 		return fmt.Errorf("unpause failed: %w", err)
 	}
 
-	p.logger.Info().Str("id", c.id).Str("image", c.imageFQN).Msg("container has been unpaused")
+	p.logger.Debug().Str("id", c.id).Str("image", c.imageFQN).Msg("container has been unpaused")
 
 	c.setStatus(statusFetched)
 
